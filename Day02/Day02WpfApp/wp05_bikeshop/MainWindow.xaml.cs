4f6c30773cc159789cdcc8e382de22f921f67d35
hugoMGSung <personar95@naver.com>
2023-04-19 17:28:24 +0900

    4월 19일 커밋

--- Day02/Day02WpfApp/wp05_bikeshop/MainWindow.xaml.cs
+++ Day02/Day02WpfApp/wp05_bikeshop/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace wp02_simplecontrols
+namespace wp05_bikeshop
 {
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
@@ -25,18 +23,6 @@ namespace wp02_simplecontrols
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void SldStep_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            PgbStep.Value = (int)SldStep.Value;
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            var path = Directory.GetCurrentDirectory();
-            MediaPlayer.Source = new Uri(path + @"\Resources\earth.avi", UriKind.RelativeOrAbsolute);
-            MediaPlayer.Play();
         }
     }
 }
